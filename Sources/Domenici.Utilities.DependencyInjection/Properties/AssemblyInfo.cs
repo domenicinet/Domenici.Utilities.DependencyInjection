4518bbca558018254e2e2e8cbbb325e0b6668b69
Alex Domenici <alex@2c2p.com>
2017-02-21 17:08:56 +0700

    Updated binaries.

--- Sources/Domenici.Utilities.DependencyInjection/Properties/AssemblyInfo.cs
+++ Sources/Domenici.Utilities.DependencyInjection/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Domenici.NET")]
 [assembly: AssemblyProduct("Domenici.Utilities.DependencyInjection")]
-[assembly: AssemblyCopyright("Copyright © 2016 Alessandro Domenici (alx.domenici@gmail.com)")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 Alessandro Domenici (alx.domenici@gmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
